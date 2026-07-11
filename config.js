5d641a45ac9c92c9be37e1ed5d917fa0315eb1f9
Dominionpr <167171847+Dominionpr@users.noreply.github.com>
2026-07-11 08:30:01 +0100

    Force latest frontend assets and AWS profile sync

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.API_BASE_URL = 'https://7rh6lrhed4.execute-api.us-east-1.amazonaws.com/Prod';
-window.PROFILE_API_URL = 'https://n19j1lw59j.execute-api.us-east-1.amazonaws.com/Prod/profile';
+window.PROFILE_API_URL = 'https://t1nxbfvmd7.execute-api.us-east-1.amazonaws.com/Prod/profile';
 window.DEFAULT_TENANT_ID = 'default';
 window.COGNITO_CONFIG = {
   region: 'us-east-1',
